1b8a6212d69d8e71c9fe1acc03bef4fe0388f381
Philip Jönsson <p2001j@gmail.com>
2017-08-18 14:13:02 +0200

    Add files via upload

--- js/funktion.js
+++ js/funktion.js
@@ -58,14 +58,23 @@ app.controller("KommentarCtrl", function($scope, kommentarer) {
 var position = $(window).scrollTop(); 
 console.log(position);
 
+var menuUpp = false;
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
-  if (scroll > 100) {
+  if (scroll > 60) {
     console.log("meny-upp");
+    $(".navbar").css("transition", "all 0.6s");
+    $(".navbar").css("margin-top", "0px");
+    $(".navbar").css("background-color", "rgba(0,0,0,0.5)");
+    menuUpp = true;
     //menuheight.style.cssText ="margin:0 !important;";           <---- Fått rätt på javascriptkoden, men inte på hur man använder css i javascript
     /*$(".nav").toggleClass("menuheight");*/
   } else {
-    console.log("meny-ner");
+    if (menuUpp) {
+      $(".navbar").css("transition", "all 0.6s");
+      $(".navbar").css("margin-top", "86px");
+      $(".navbar").css("background-color", "transparent");
+    }
   }
   console.log(position);
   position = scroll;
